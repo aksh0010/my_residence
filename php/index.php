d52c5119fa433b42d8f82e0e248762922dd84270
liamgorey11 <57719630+liamgorey11@users.noreply.github.com>
2022-11-22 15:38:12 -0500

    added some basic text for about us and home page
    Also changed up the design a bit and added a footer.
    let me know if I should change anything.

--- php/index.php
+++ php/index.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 
     <!-- CSS only -->
     <link
@@ -21,20 +24,123 @@
     ></script>
   </head>
   <style>
+    /*make navbar a diffrent color*/
+    .container {
+      background-color: rgba(255, 255, 255, 0.5);
+      border-radius: 10px;
+      padding: 10px;
+      margin-top: 30px;
+      background-color: #04aa6d;
+      margin-bottom: 30px;
+    }
+    /*container that extends other container with a diffretn background color*/
+    .container2 {
+      background-color: rgba(255, 255, 255, 0.5);
+      border-radius: 10px;
+      padding: 10px;
+      margin-left: 50px;
+      margin-right: 50px;
+      background-color: #17735a;
+    }
     .body-bg {
       background-image: url("./img/img1.jpg");
-      padding: 5px;
       /* border: 2px solid black; */
       background-repeat: no-repeat;
       background-attachment: fixed;
       background-size: 100%;
     }
+    .footer-basic {
+      margin-top: 30px;
+      padding:40px 0;
+      background-color:#17735a;
+    }
+
+    .footer-basic ul {
+      color:000000;
+      padding:0;
+      list-style:none;
+      text-align:center;
+      font-size:18px;
+      line-height:1.6;
+      margin-bottom:0;
+    }
+
+    .footer-basic li {
+      color:000000;
+      padding:0 10px;
+    }
+
+    .footer-basic ul a {
+      color:000000;
+      text-decoration:none;
+      opacity:0.8;
+    }
+
+    .footer-basic ul a:hover {
+      opacity:1;
+      color:000000;
+    }
+
+    .footer-basic .social {
+      text-align:center;
+      padding-bottom:25px;
+      color:000000;
+    }
+
+    .footer-basic .social > a {
+      font-size:24px;
+      width:40px;
+      height:40px;
+      line-height:40px;
+      display:inline-block;
+      text-align:center;
+      border-radius:50%;
+      border:1px solid #ccc;
+      margin:0 8px;
+      color:000000;
+      opacity:0.75;
+    }
+
+    .footer-basic .social > a:hover {
+      opacity:0.9;
+      color:000000;
+    }
+
+    .footer-basic .copyright {
+      margin-top:15px;
+      text-align:center;
+      font-size:13px;
+      margin-bottom:0;
+      color:000000;
+    }
+
+    .text-center{
+      padding:15px;
+    }
+    .text-center2{
+      padding:15px;
+      font-style: italic;
+      font-size: 20px;
+      text-align:center;
+    }
+    .img-fluid{
+      size: 120%;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      width: 65%;
+      border: 2px solid black;
+      border-radius:47%;
+    }
+    .navbar-custom{
+      background-color: #17735a;
+    }
   </style>
 
-  <body class="body-bg">
+<body class="body-bg">
       <!-- !! Div for navigation bar -->
-      <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">My Residence</a>
           <button
@@ -70,6 +176,91 @@
       </nav>
     </div>
     <!-- !! Div for rest content  -->
-    <div></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h1 class="text-center">Welcome to My Residence</h1>
+          <h3 class="text-center2">"Your Home Away From Home"</h3>
+        </div>
+      </div>
+    </div>
+    <!-- !!content for home page of university residence page  -->
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h1 class="text-center">What we Provide</h1>
+          <p class="text-center">
+            We provide a safe and secure environment for students to live in
+            while attending university. We have a variety of rooms to choose from
+            and a variety of amenities to choose from. We have rooms on campus in our main residence buildings aswell 
+            as off campus in our off campus residence buildings. These of campus rooms are located in the downtown area 
+            of the city but still within a good walking distnace from campus.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- !!pictues of some rooms for residence with some nice padding and borders around them vertical on the website with description boxes underneath each image-->
+    <div class = "container">
+        <h1 class="text-center">Some of our available rooms</h1>
+        <div class = "row">
+        <div class="col-sm-4">
+          <div class = "container2">
+            <img src="./img/room1.jpg" class="img-fluid" alt="Responsive image">
+          </div>
+          <p class="text-center">Room in our main residence hall</p>
+          
+        </div>
+        <div class="col-sm-4">
+          <div class = "container2">
+            <img src="./img/room2.jpg" class="img-fluid" alt="Responsive image">
+          </div>
+          <p class="text-center">Room in secondary hall</p>
+        </div>
+        <div class="col-sm-4">
+          <div class = "container2">
+            <img src="./img/room3.jpg" class="img-fluid" alt="Responsive image">
+          </div>
+          <p class="text-center">Room in our bad hall</p>
+        </div>
+      </div>
+      </div>  
+    </div>
+    <!-- More information for the details about how the website works with the froms and querying the database  -->
+    <div class = "container">
+      <div class = "row">
+        <div class="col-sm-12">
+          <h1 class="text-center">How it works</h1>
+          <p class="text-center">
+            We have a database that stores all the information about the rooms
+            and the students that live in them. We have a form that allows students 
+            to search for a room based on the price of the room. It will search both 
+            the student flats and dorm rooms avaialbe. A advisor can manage the 
+            student that is assigned to them. They can add a student to their list aswell.
+            As an admin can do many difftent DDL and DML commands to the database.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- !! Div for footer  -->  
+    <footer class="footer-basic">
+      <div class="social">
+        <a href="#" style="color:black;"><i class="icon ion-social-instagram" style="color:black;"></i></a>
+        <a href="#" style="color:black;"><i class="icon ion-social-snapchat" style="color:black;"></i></a>
+        <a href="#" style="color:black;"><i class="icon ion-social-twitter" style="color:black;"></i></a>
+        <a href="#" style="color:black;"><i class="icon ion-social-facebook" style="color:black;"></i></a>
+      </div>
+      <div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="./index.php" style="color:black;">Home</a></li>
+            <li class="list-inline-item"><a href="#" style="color:black;">Services</a></li>
+            <li class="list-inline-item"><a href="./about_us.html" style="color:black;">About</a></li>
+            <li class="list-inline-item"><a href="#" style="color:black;">Terms</a></li>
+            <li class="list-inline-item"><a href="#" style="color:black;">Privacy Policy</a></li>
+        </ul>
+      </div>
+      <div>
+        <p class="copyright">MyResidence © 2018</p>
+      </div>
+    </footer>
   </body>
 </html>
--- php/login_page.php
+++ php/login_page.php
@@ -10,6 +10,9 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
   <!-- JavaScript Bundle with Popper  -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <style>
   body {
@@ -92,6 +95,14 @@
     /* Black w/ opacity */
     padding-top: 60px;
   }
+  .container {
+      background-color: rgba(255, 255, 255, 0.5);
+      border-radius: 10px;
+      padding: 10px;
+      margin-top: 30px;
+      background-color: #04aa6d;
+      margin-bottom: 30px;
+    }
 
   /* Modal Content/Box */
   .modal-content {
@@ -159,212 +170,317 @@
 
   .body-bg {
     background-image: url("./img/img1.jpg");
-    padding: 5px;
     /* border: 2px solid black; */
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-size: 100%;
   }
+  .footer-basic {
+      margin-top: 30px;
+      padding:40px 0;
+      background-color:#17735a;
+    }
+
+    .footer-basic ul {
+      color:000000;
+      padding:0;
+      list-style:none;
+      text-align:center;
+      font-size:18px;
+      line-height:1.6;
+      margin-bottom:0;
+    }
+
+    .footer-basic li {
+      color:000000;
+      padding:0 10px;
+    }
+
+    .footer-basic ul a {
+      color:000000;
+      text-decoration:none;
+      opacity:0.8;
+    }
+
+    .footer-basic ul a:hover {
+      opacity:1;
+      color:000000;
+    }
+
+    .footer-basic .social {
+      text-align:center;
+      padding-bottom:25px;
+      color:000000;
+    }
+
+    .footer-basic .social > a {
+      font-size:24px;
+      width:40px;
+      height:40px;
+      line-height:40px;
+      display:inline-block;
+      text-align:center;
+      border-radius:50%;
+      border:1px solid #ccc;
+      margin:0 8px;
+      color:000000;
+      opacity:0.75;
+    }
+
+    .footer-basic .social > a:hover {
+      opacity:0.9;
+      color:000000;
+    }
+
+    .footer-basic .copyright {
+      margin-top:15px;
+      text-align:center;
+      font-size:13px;
+      margin-bottom:0;
+      color:000000;
+    }
+
+    .text-center{
+      padding:15px;
+    }
+    .text-center2{
+      padding:15px;
+      font-style: italic;
+      font-size: 20px;
+      text-align:center;
+    }
+    .img-fluid{
+      size: 120%;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      width: 65%;
+      border: 2px solid black;
+      border-radius:47%;
+    }
+    .navbar-custom{
+      background-color: #17735a;
+    }
 </style>
 
 <body class="body-bg">
-    <!-- !! Div for navigation bar -->
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">My Residence</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a
-                  class="nav-link active"
-                  aria-current="page"
-                  href="./index.php"
-                  >Home</a
-                >
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./login_page.php">Login</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./about_us.html">About us </a>
-              </li>
-            </ul>
-          </div>
+  <!-- !! Div for navigation bar -->
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">My Residence</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="./index.php"
+                >Home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./login_page.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./about_us.html">About us </a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
+  </div>
   <!-- !!  Div for the rest of the content  -->
   <!-- !!  My Residence Login  -->
-  <div class="list-group ">
-    <br><br>
-    <h1 style="text-align: center">Login to your MyResidence Account</h1>
-    <br><br>
-    <!-- !!  add above this text-bg-warning p-3 to make it yellow -->
-    <a href="./login_home.php" class="list-group-item list-group-item-action" style="background-color: #6082B6;" aria-current="true">
-      My Residence Login
-    </a>
-    <!-- !!  Admin Login  -->
-    <a href="#" class="list-group-item list-group-item-action"><button onclick="document.getElementById('id01').style.display='block'" style="width: auto">
-        Admin
-      </button>
+  <div class= "container">
+    <div class="list-group ">
+      <br><br>
+      <h1 style="text-align: center">Login to your MyResidence Account</h1>
+      <br><br>
+      <!-- !!  add above this text-bg-warning p-3 to make it yellow -->
+      <a href="./login_home.php" class="list-group-item list-group-item-action" style="background-color: #6082B6;" aria-current="true">
+        My Residence Login
+      </a>
+      <!-- !!  Admin Login  -->
+      <a href="#" class="list-group-item list-group-item-action"><button onclick="document.getElementById('id01').style.display='block'" style="width: auto">
+          Admin
+        </button>
 
-      <div id="id01" class="modal">
-        <form class="modal-content animate" action="./admin_auth.php" method="POST">
-          <!-- action="/admin_login.php" -->
-          <div class="imgcontainer">
-            <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-            <img src="./img/admin.png" alt="Avatar" class="avatar" />
-          </div>
+        <div id="id01" class="modal">
+          <form class="modal-content animate" action="./admin_auth.php" method="POST">
+            <!-- action="/admin_login.php" -->
+            <div class="imgcontainer">
+              <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+              <img src="./img/admin.png" alt="Avatar" class="avatar" />
+            </div>
 
-          <div class="container">
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required />
+            <div class="container">
+              <label for="uname"><b>Username</b></label>
+              <input type="text" placeholder="Enter Username" name="uname" required />
 
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required />
+              <label for="psw"><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" name="psw" required />
 
-            <button type="submit">Login</button>
-            <label>
-              <input type="checkbox" checked="checked" name="remember" />
-              Remember me
-            </label>
-          </div>
+              <button type="submit">Login</button>
+              <label>
+                <input type="checkbox" checked="checked" name="remember" />
+                Remember me
+              </label>
+            </div>
 
-          <div class="container" style="background-color: #f1f1f1">
-            <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">
-              Cancel
-            </button>
-            <!-- <span class="psw">Forgot <a href="#">password?</a></span> -->
-          </div>
-        </form>
-      </div>
+            <div class="container" style="background-color: #f1f1f1">
+              <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">
+                Cancel
+              </button>
+              <!-- <span class="psw">Forgot <a href="#">password?</a></span> -->
+            </div>
+          </form>
+        </div>
 
-      <script>
-        // Get the modal
-        var modal = document.getElementById("id01");
+        <script>
+          // Get the modal
+          var modal = document.getElementById("id01");
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        };
-      </script>
-    </a>
-    <!-- !!  Student Login  -->
-    <a href="#" class="list-group-item list-group-item-action"><button onclick="document.getElementById('id02').style.display='block'" style="width: auto">
-        Student
-      </button>
+          // When the user clicks anywhere outside of the modal, close it
+          window.onclick = function(event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+            }
+          };
+        </script>
+      </a>
+      <!-- !!  Student Login  -->
+      <a href="#" class="list-group-item list-group-item-action"><button onclick="document.getElementById('id02').style.display='block'" style="width: auto">
+          Student
+        </button>
 
-      <div id="id02" class="modal">
-        <form class="modal-content animate" action="./student_auth.php" method="POST">
-          <div class="imgcontainer">
-            <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
-            <img src="./img/student.png" alt="Avatar" class="avatar" />
-          </div>
+        <div id="id02" class="modal">
+          <form class="modal-content animate" action="./student_auth.php" method="POST">
+            <div class="imgcontainer">
+              <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
+              <img src="./img/student.png" alt="Avatar" class="avatar" />
+            </div>
 
-          <div class="container">
-          <label for="uname"><b>Student ID</b></label>
-            <input type="text" placeholder="Enter Student Id" name="std_id" required />
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required />
+            <div class="container">
+            <label for="uname"><b>Student ID</b></label>
+              <input type="text" placeholder="Enter Student Id" name="std_id" required />
+              <label for="uname"><b>Username</b></label>
+              <input type="text" placeholder="Enter Username" name="uname" required />
 
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required />
+              <label for="psw"><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" name="psw" required />
 
-            <button type="submit">Login</button>
-            <label>
-              <input type="checkbox" checked="checked" name="remember" />
-              Remember me
-            </label>
-          </div>
+              <button type="submit">Login</button>
+              <label>
+                <input type="checkbox" checked="checked" name="remember" />
+                Remember me
+              </label>
+            </div>
 
-          <div class="container" style="background-color: #f1f1f1">
-            <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">
-              Cancel
-            </button>
-            <!-- <span class="psw">Forgot <a href="#">password?</a></span> -->
-          </div>
-        </form>
-      </div>
+            <div class="container" style="background-color: #f1f1f1">
+              <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">
+                Cancel
+              </button>
+              <!-- <span class="psw">Forgot <a href="#">password?</a></span> -->
+            </div>
+          </form>
+        </div>
 
-      <script>
-        // Get the modal
-        var modal = document.getElementById("id02");
+        <script>
+          // Get the modal
+          var modal = document.getElementById("id02");
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        };
-      </script>
-    </a>
-    <a href="#" class="list-group-item list-group-item-action"><button onclick="document.getElementById('id03').style.display='block'" style="width: auto">
-        <!-- !!  Staff Login  -->
-        Staff
-      </button>
+          // When the user clicks anywhere outside of the modal, close it
+          window.onclick = function(event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+            }
+          };
+        </script>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action"><button onclick="document.getElementById('id03').style.display='block'" style="width: auto">
+          <!-- !!  Staff Login  -->
+          Staff
+        </button>
 
-      <div id="id03" class="modal">
-        <form class="modal-content animate" action="./staff_auth.php" method="POST">
-          <div class="imgcontainer">
-            <span onclick="document.getElementById('id03').style.display='none'" class="close" title="Close Modal">&times;</span>
-            <img src="./img/staff.png" alt="Avatar" class="avatar" />
-          </div>
+        <div id="id03" class="modal">
+          <form class="modal-content animate" action="./staff_auth.php" method="POST">
+            <div class="imgcontainer">
+              <span onclick="document.getElementById('id03').style.display='none'" class="close" title="Close Modal">&times;</span>
+              <img src="./img/staff.png" alt="Avatar" class="avatar" />
+            </div>
 
-          <div class="container">
-          <label for="uname"><b>Staff ID</b></label>
-            <input type="text" placeholder="Enter Staff Id" name="staff_num" required />
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required />
+            <div class="container">
+            <label for="uname"><b>Staff ID</b></label>
+              <input type="text" placeholder="Enter Staff Id" name="staff_num" required />
+              <label for="uname"><b>Username</b></label>
+              <input type="text" placeholder="Enter Username" name="uname" required />
 
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required />
+              <label for="psw"><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" name="psw" required />
 
-            <button type="submit">Login</button>
-            <label>
-              <input type="checkbox" checked="checked" name="remember" />
-              Remember me
-            </label>
-          </div>
+              <button type="submit">Login</button>
+              <label>
+                <input type="checkbox" checked="checked" name="remember" />
+                Remember me
+              </label>
+            </div>
 
-          <div class="container" style="background-color: #f1f1f1">
-            <button type="button" onclick="document.getElementById('id03').style.display='none'" class="cancelbtn">
-              Cancel
-            </button>
-            <!-- <span class="psw">Forgot <a href="#">password?</a></span> -->
-          </div>
-        </form>
-      </div>
+            <div class="container" style="background-color: #f1f1f1">
+              <button type="button" onclick="document.getElementById('id03').style.display='none'" class="cancelbtn">
+                Cancel
+              </button>
+              <!-- <span class="psw">Forgot <a href="#">password?</a></span> -->
+            </div>
+          </form>
+        </div>
+          
+        <script>
+          // Get the modal
+          var modal = document.getElementById("id03");
 
-      <script>
-        // Get the modal
-        var modal = document.getElementById("id03");
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        };
-      </script>
-    </a>
-    <!-- !! Back to home page  -->
-   
+          // When the user clicks anywhere outside of the modal, close it
+          window.onclick = function(event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+            }
+          };
+        </script>
+      </a>
+      <!-- !! Back to home page  --> 
+    </div>
   </div>
-  </div>
+  <footer class="footer-basic">
+      <div class="social">
+        <a href="#" style="color:black;"><i class="icon ion-social-instagram" style="color:black;"></i></a>
+        <a href="#" style="color:black;"><i class="icon ion-social-snapchat" style="color:black;"></i></a>
+        <a href="#" style="color:black;"><i class="icon ion-social-twitter" style="color:black;"></i></a>
+        <a href="#" style="color:black;"><i class="icon ion-social-facebook" style="color:black;"></i></a>
+      </div>
+      <div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="./index.php" style="color:black;">Home</a></li>
+            <li class="list-inline-item"><a href="#" style="color:black;">Services</a></li>
+            <li class="list-inline-item"><a href="./about_us.html" style="color:black;">About</a></li>
+            <li class="list-inline-item"><a href="#" style="color:black;">Terms</a></li>
+            <li class="list-inline-item"><a href="#" style="color:black;">Privacy Policy</a></li>
+        </ul>
+      </div>
+      <div>
+        <p class="copyright">MyResidence © 2018</p>
+      </div>
+    </footer>
 </body>
 
 </html>
